da0202c28c58b35abdb2e44e7f3ee2572c4d8ef4
Adam Powell <apowell@umass.edu>
2020-07-22 16:08:42 -0400

    Make monitor work

---
+++ coolermathgames-project/Assets/Monitor/MonitorController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonitorController : MonoBehaviour {
+
+
+    public GameObject OSPrefab;
+
+    // Create a camera / OSController / rendertexture to use for this monitor, then assign it.
+    // Called by ComputerTurnOner to populate every monitor in the scene.
+    public void SetupScreen(Vector3 offset)
+    {
+        // Create an instance of the chosen OS
+        GameObject OS = Instantiate(OSPrefab, offset, Quaternion.identity);
+        
+        // Assign the render texture to place the OS onto the monitor
+        RenderTexture rt = new RenderTexture(320, 240, 16, RenderTextureFormat.ARGB32);
+        rt.filterMode = FilterMode.Point;
+        OS.transform.Find("camera").GetComponent<Camera>().targetTexture = rt;
+        this.GetComponent<MeshRenderer>().sharedMaterial.mainTexture = rt;
+    }
+}
---
+++ coolermathgames-project/Assets/Monitor/MonitorTurnOner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonitorTurnOner : MonoBehaviour
+{
+
+    public int offsetX = 10;
+    public int offsetY = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 curOffset = new Vector3(0, 0, 0);
+        GameObject[] monitors = GameObject.FindGameObjectsWithTag("Monitor");
+        foreach (GameObject monitor in monitors)
+        {
+            curOffset += new Vector3(offsetX, offsetY, 0);
+            monitor.GetComponent<MonitorController>().SetupScreen(curOffset);
+        }
+    }
+}

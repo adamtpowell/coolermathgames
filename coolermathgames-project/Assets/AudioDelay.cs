6f97870040a623551eecadffcbe34dcf0975cc97
Jerry Zeng <56747635+jlzeng-i-got-locked-out-of-rokky258@users.noreply.github.com>
2020-07-22 17:32:48 -0400

    sounds

---
+++ coolermathgames-project/Assets/AudioDelay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioDelay : MonoBehaviour
+{
+    public float delay = 0f;
+     public AudioClip otherClip;
+
+    IEnumerator Start()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+
+        audio.PlayDelayed(delay);
+        yield return new WaitForSeconds(audio.clip.length);
+    }
+}

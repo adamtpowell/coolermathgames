574b690d9deaaa0f6dd5039e79ce1a0c6d6ad9af
Adam Powell <apowell@umass.edu>
2020-07-23 16:54:08 -0400

    WINGDONE

--- coolermathgames-project/Assets/OS/WingdingsGame.cs
+++ coolermathgames-project/Assets/OS/WingdingsGame.cs
@@ -7,20 +7,15 @@ using UnityEngine.UI;
 public class WingdingsGame : MonoBehaviour
 {
 
-    private float secondsLeft = 60;
+    private float secondsLeft = 0;
     private int score = 0;
-    private bool running = true;
+    private bool running = false;
     private char currentWingding;
 
     public TextMeshPro scoreLabel;
     public TextMeshPro timeLabel;
     public TextMeshPro wingdingLabel;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        currentWingding = GetDing();
-    }
+    public TextMeshPro gameoverLabel;
 
     private char GetDing()
     {
@@ -33,28 +28,49 @@ public class WingdingsGame : MonoBehaviour
         timeLabel.text = Mathf.Floor(secondsLeft).ToString();
         scoreLabel.text = score.ToString();
         wingdingLabel.text = currentWingding.ToString();
+
+        gameoverLabel.GetComponent<MeshRenderer>().enabled = !running;
+        wingdingLabel.GetComponent<MeshRenderer>().enabled = running;
+
+    }
+
+    private void RestartGame()
+    {
+        secondsLeft = 60f;
+        score = 0;
+        running = true;
+        currentWingding = GetDing();
     }
 
     // Update is called once per frame
     void Update()
     {
         UIReact();
-        foreach (KeyCode keyCode in System.Enum.GetValues(typeof(KeyCode)))
+        if (running)
         {
-            char wingdingLower = System.Char.ToLower(currentWingding);
-            bool isShiftPressed = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
-            bool isKeycodePressed = Input.GetKey(keyCode);
-            if ((wingdingLower == currentWingding && (char)keyCode == wingdingLower && !isShiftPressed && isKeycodePressed) || // lowercase
-                (wingdingLower != currentWingding && (char)keyCode == wingdingLower && isShiftPressed && isKeycodePressed)) // uppercase
+            foreach (KeyCode keyCode in System.Enum.GetValues(typeof(KeyCode)))
             {
-                score += 100;
-                currentWingding = GetDing();
+                char wingdingLower = System.Char.ToLower(currentWingding);
+                bool isShiftPressed = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+                bool isKeycodePressed = Input.GetKey(keyCode);
+                if ((wingdingLower == currentWingding && (char)keyCode == wingdingLower && !isShiftPressed && isKeycodePressed) || // lowercase
+                    (wingdingLower != currentWingding && (char)keyCode == wingdingLower && isShiftPressed && isKeycodePressed)) // uppercase
+                {
+                    score += 100;
+                    currentWingding = GetDing();
+                }
+            }
+        } else
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                RestartGame();
             }
         }
-
+        
         if (secondsLeft < 0.0f)
         {
-            //running = false;
+            running = false;
         }
         secondsLeft = running ? secondsLeft - Time.deltaTime : 0.0f;
     }

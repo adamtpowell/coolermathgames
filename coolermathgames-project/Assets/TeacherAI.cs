8758be93a909718013c9570525d27d5617215c0b
Jerry Zeng <56747635+jlzeng-i-got-locked-out-of-rokky258@users.noreply.github.com>
2020-07-23 16:58:27 -0400

    added box collider and restricted turning on AI

--- coolermathgames-project/Assets/TeacherAI.cs
+++ coolermathgames-project/Assets/TeacherAI.cs
@@ -6,7 +6,7 @@ public class TeacherAI : MonoBehaviour
 {
 
     int rotation = 0;
-
+    public int viewlock = 75;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +17,12 @@ public class TeacherAI : MonoBehaviour
     void Update()
     {
         //This is just for testing out moving the teacher's FOV around and testing collisions with the computer and other things
+    
        rotation += Random.Range(-10,10);
+
+       if (rotation < -viewlock ) rotation = -viewlock;
+       if (rotation > viewlock) rotation = viewlock;
+
        transform.localRotation = Quaternion.Euler(0f, rotation, 265f);
     }
 }

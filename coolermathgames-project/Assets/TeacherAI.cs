5c6b059751d1a3b68ef8ed80da7005e0955c9e01
Jerry Zeng <56747635+jlzeng-i-got-locked-out-of-rokky258@users.noreply.github.com>
2020-07-23 17:36:39 -0400

    I dont really know what I changed honestly

--- coolermathgames-project/Assets/TeacherAI.cs
+++ coolermathgames-project/Assets/TeacherAI.cs
@@ -2,11 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TeacherAI : MonoBehaviour
+public class teacherAI : MonoBehaviour
 {
-
-    int rotation = 0;
-    public int viewlock = 75;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,16 +13,6 @@ public class TeacherAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //This is just for testing out moving the teacher's FOV around and testing collisions with the computer and other things
-    
-       rotation += Random.Range(-10,10);
-
-       if (rotation < -viewlock ) rotation = -viewlock;
-       if (rotation > viewlock) rotation = viewlock;
-
-       transform.localRotation = Quaternion.Euler(0f, rotation, 265f);
-    }
-    void OnCollisionEnter(Collision other) {
-        print("Detected collision");
+        
     }
 }
---
+++ coolermathgames-project/Assets/visionAI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class visionAI : MonoBehaviour
+{
+
+    int rotation = 0;
+    public int viewlock = 75;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This is just for testing out moving the teacher's FOV around and testing collisions with the computer and other things
+    
+       rotation += Random.Range(-10,10);
+
+       if (rotation < -viewlock ) rotation = -viewlock;
+       if (rotation > viewlock) rotation = viewlock;
+
+       transform.localRotation = Quaternion.Euler(0f, rotation, 265f);
+    }
+    void OnCollisionEnter(Collision other) {
+        print("Detected collision");
+
+        //Consider redoing collision and doing raycasting instead
+    }
+}
